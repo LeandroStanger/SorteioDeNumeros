963c4f8c299c108803ccd654e2ebaf6a9114b78a
Leandro Stanger <leandrostanger@aol.com>
2026-03-22 12:09:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let sortearBtn = document.getElementById("sortearBtn");
+let outputEl = document.getElementById("output");
+let form = document.getElementById("numeroDoUsuario");
+let numero = document.getElementById("numero");
+
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+})
+
+sortearBtn.addEventListener("click", () => {
+    outputEl.textContent = "";
+
+
+    let valorDigitado = parseInt(numero.value, 10);
+
+    let sorteio = Math.floor(Math.random() * (valorDigitado + 1 || 100));
+    outputEl.textContent = sorteio;
+})
